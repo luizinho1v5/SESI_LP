5f583bc8fc7d84558934b8176639d5bf7d920d62
Luiz <bingodv@gmail.com>
2026-04-27 10:43:05 -0300

    aula06

---
+++ aula_06/atividade11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float num[10];
+     float temp;
+
+    for(int i = 0; i < 10; i++){
+        printf("Digite numero %i: ", i+1);
+        scanf("%f", &num[i]);
+
+}
+for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 9; j++){
+            if(num[j] < num[j+1]){
+             
+                temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+    }
+    
+    printf("\nNumeros em ordem decrescente:\n");
+    for(int i = 0; i < 10; i++){
+        printf("%.2f ", num[i]);
+    }
+}
